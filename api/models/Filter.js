6137ae2e4a31e51ae123ffdbae394264a4998908
yasuo0827 <mnblkjpoi0324@hotmail.com>
2020-08-16 02:27:33 +0200

    updating

--- api/models/Filter.js
+++ api/models/Filter.js
@@ -5,80 +5,54 @@ const Schema = mongoose.Schema;
  * Brand schema
  */
 const FilterSchema = new Schema({
-    // product_photo: {
-    //     type: String,
-    //     required: false,
-    // },
-    //
-    // product_id: {
-    //     type: String,
-    //     required: false,
-    // },
-    //
-    // product_category: {
-    //     type: String,
-    //     required: false,
-    // },
-    //
-    // product_name: {
-    //     type: String,
-    //     required: false,
-    // },
-    //
-    // product_description: {
-    //     type: String,
-    //     required: false,
-    // },
-    //
-    // product_price: {
-    //     type: Number,
-    //     required: false,
-    // },
-    //
-    // product_store_address: {
-    //     type: String,
-    //     required: false,
-    // },
-
-    scraping_id: {
+    id: {
         type: String,
         required: true,
     },
-
-    scraping_store_address: {
+    link: {
         type: String,
         required: true,
     },
-
-    scraping_photo_link: {
-        type: String,
-        required: true,
-    },
-
-    scraping_category: {
+    category: {
         type: String,
         required: false,
     },
-
-    scraping_name: {
+    country: {
         type: String,
         required: false,
     },
-
-    scraping_description: {
+    league: {
         type: String,
         required: false,
     },
-
-    scraping_price: {
-        type: Number,
-        required: false,
-    },
-
-    scraping_thumbnail_Link : {
+    matchYear: {
         type: String,
         required: false,
-    }
+    },
+    teamA: {
+        type: String,
+        required: false,
+    },
+    teamB: {
+        type: String,
+        required: false,
+    },
+    matchDate: {
+        type: String,
+        required: false,
+    },
+    finalScore: {
+        type: String,
+        required: false,
+    },
+    historiesFirstHalf: {
+        type: String,
+        required: false,
+    },
+    historiesSecondHalf: {
+        type: String,
+        required: false,
+    },
 });
 
 module.exports = Filter = mongoose.model("filters", FilterSchema);
--- api/routes/scrapingProduct.js
+++ api/routes/scrapingProduct.js
@@ -9,6 +9,9 @@ const axios = require("axios");
 const request = require('request');
 const cheerio = require('cheerio');
 
+const wget = require('wget');
+
+
 let goLink = [];
 let mLen, timeFlag;
 
@@ -17,9 +20,7 @@ let firstCategory = "div.menuMinority > div.menuMinority__content > div.menuMino
 let secondCountry = "div.mbox0px > ul.menu.country-list.tournament-menu > li > a";
 let thirdLeague = "div.mbox0px > ul.menu.selected-country-list > li > a";
 let forthSeason = "div.leagueTable__season > div.leagueTable__seasonName > a";
-let fifthMatch = "#live-table";
-
-//let fifthMatch = "div.sportName > div.event__match";
+let fifthMatch = "sportName";
 
 
 let nCategory = 0;
@@ -29,6 +30,7 @@ let nSeason = 0;
 let nTeams = 0;
 
 let lastStage = false;
+let lastId = 0;
 let goLinkLen = 2688;
 
 const nth = 1;
@@ -36,6 +38,7 @@ let nCount_First = 1, nCount_Products;
 
 let m = 0;
 let pStage = 0;
+
 
 router.post("/scraping-product", async (req, res) => {
     await console.log("--------------- success  ----------------");
@@ -115,19 +118,27 @@ router.post("/scraping-product", async (req, res) => {
      * Getting Description
      */
     await ScrapingProduct.find({}).then(async scrapingItem => {
-        lastStage = true;
+
         let pLen = scrapingItem.length;
 
         for (let k = nSeason; k < pLen; k ++) {
+            lastStage = true;
+            await sleep(2000);
+            await console.log("k = ", k, '\n', scrapingItem[k].link);
 
             await gettingCategoryLink(fifthMatch, scrapingItem[k].link + "risultati/");
+            // await console.log("###############", "5 Stage/", k, "  -->  ", goLink.length);
 
-            console.log("###############", "5 Stage/", k, "  -->  ", goLink.length);
+
+
+
+
+
+            /*let linkS = "https://www.diretta.it/volley/croazia/superliga-2011-2012/risultati/";
+            const outputFile = "aaa.txt";
+            wget.download(linkS, outputFile);
+            await gettingCategoryLink(fifthMatch, linkS);*/
         }
-
-        // let pLen = "https://www.diretta.it/basket/australia/wnbl-women-2018-2019/";
-        // await gettingCategoryLink(fifthMatch, pLen + "risultati/");
-        // console.log("###############", "5 Stage/", 12312, "  -->  ", goLink.length);
     });
 
     nTeams = goLink.length;
@@ -139,8 +150,9 @@ router.post("/scraping-product", async (req, res) => {
 
 
 
+
     // await gettingCountryLeagueLink(secondCountryLeague);
-    await console.log(" ===============  Country and League Scraping Done !!!!! =============");
+    console.log(" ===============  Country and League Scraping Done !!!!! =============");
     // console.log(goLink, "categoryCount = ", nCategory);
 
 
@@ -201,50 +213,189 @@ async function initializeDB(pStage) {
  */
 async function gettingCategoryLink(matchStr, bUrl) {
     try {
-        const result = await axios.get(bUrl);
-        let $ = await cheerio.load(result.data);
-        let aStr;
-        const aTags = $(matchStr);
+        if (lastStage === true) {
+            try {
+                require('chromedriver');
+                const webdriver = require('selenium-webdriver');
+                let By = webdriver.By;
+                let until = webdriver.until;
+                const driver = new webdriver.Builder()
+                    .forBrowser('chrome')
+                    .build();
 
-        console.log('Len = ', aTags.length);
-        // console.log('aTags = ', aTags);
-        // console.log("bUrl ->", bUrl);
+                await driver.get("https://www.diretta.it/calcio/albania/super-league/risultati/");
+                await driver.wait(until.elementLocated(By.className(matchStr)));
+                let sMatch = await driver.findElement(By.className(matchStr)).getAttribute('innerHTML');
+                console.log("successful  -> ", sMatch);
+                await driver.quit();
+                console.log(sMatch.length);
 
-        // for (let i = 0; i < aTags.length; i++) {console.log("Scraping-5th");
-        //     if(lastStage === true) {
-        //         // let sMiddle = aTags[i].find("div.event");
-        //         // console.log(sMiddle);
-        //         // let id = aTags[i].attr("id");
-        //         // if(id.includes("g_") === false) break;
-        //         //
-        //         // if(id[3] === "_") {
-        //         //     aStr = id.slice(3, );
-        //         // } else if(id[4] === "_") {
-        //         //     aStr = id.slice(4, );
-        //         // }
-        //         //
-        //         // aStr = "/partita/" + aStr + "/#informazioni-partita/";
-        //         //
-        //         // console.log("#######", aStr);
-        //     } else {
-        //         aStr = aTags[i].attribs.href;
-        //     }
-        //
-        //     if (aStr !== undefined) {
-        //         if((aStr.includes("2003-2004") === true) || (aStr.includes("-2004") === true)) break;
-        //         mLen = goLink.length;
-        //         goLink.push('https://www.diretta.it' + aStr);
-        //         const scraping_Product = new ScrapingProduct({
-        //             id: goLink.length,
-        //             link: goLink[goLink.length - 1]
-        //         });
-        //
-        //         await scraping_Product.save();
-        //
-        //         console.log(goLink.length, "  -> ", goLink[goLink.length - 1]);
-        //         // nCategory = goLink.length;
-        //     }
-        // }
+                /**
+                 * Getting the last Link
+                 */
+                let sCategory;
+                let sCountry;
+                let sLeague;
+
+                let ss = bUrl.split("/");
+                sCategory = ss[3];
+                sCountry = ss[4];
+                sLeague = ss[5];
+
+                if(sLeague.includes("-20") === false) {
+                    sLeague += "-2019-2020";
+                }
+
+                let sId = "id=\"g_";
+                let n = sMatch.search(sId);
+
+                while(n >= 0) {
+                    try {
+                        sMatch = sMatch.slice(n + 8, );
+                        if(sMatch[0] === "_") sMatch = sMatch.slice(1, );
+
+                        let nI = sMatch.search("\"");
+                        let lastLink = sMatch.slice(0, nI);
+                        sMatch = sMatch.slice(nI + 1, );
+
+                        lastLink = baseUrl + "partita/" + lastLink + "/#informazioni-partita/";
+                        console.log(lastLink);
+
+                        /**
+                         * lastLink is the last description link
+                         * Store to the database ScrapingProducts
+                         */
+                        // goLink.push(lastLink);
+                        // const scraping_Product = new ScrapingProduct({
+                        //     id: goLink.length,
+                        //     link: goLink[goLink.length - 1]
+                        // });
+                        //
+                        // await scraping_Product.save();
+                        // console.log(goLink.length, "  -> ", goLink[goLink.length - 1]);
+
+                        /**
+                         * Description Information
+                         */
+
+                        let sDate;
+                        let sTeamA;
+                        let sTeamB;
+                        let finalScore;
+                        // let firstHalfScore;
+                        // let secondHalfScore;
+                        let timeScored_firstHalf;
+                        let timeScored_secondHalf;
+
+                        const result = await axios.get(lastLink);
+                        let $ = await cheerio.load(result.data);
+
+                        // let sCountryLeague = $("span.description__country").text();
+                        //
+                        // let t = sCountryLeague.search(":");
+                        // sCountry = sCountryLeague.slice(0, t).trim();
+                        // sLeague = sCountryLeague.slice(t + 2, ).trim();
+
+                        sTeamA = $("div.team-text.tname-home > div > div > a").text();
+                        sTeamB = $("div.team-text.tname-away > div > div > a").text();
+                        finalScore = $("div.match-info > div.current-result").text().trim();
+
+                        /**
+                         * getting the match date
+                         */
+                        require('chromedriver');
+                        const webdriver = require('selenium-webdriver');
+                        let By = webdriver.By;
+                        let until = webdriver.until;
+                        const driver = new webdriver.Builder()
+                            .forBrowser('chrome')
+                            .build();
+
+                        await driver.get(lastLink);
+                        await driver.wait(until.elementLocated(By.id("detcon")));
+
+                        sDate = await driver.findElement(By.className("description__time")).getAttribute('innerHTML');
+                        timeScored_firstHalf = await driver.findElement(By.className("time-box")).getAttribute('innerHTML');
+
+                        // await driver.wait(until.elementLocated(By.className("div.stage-12")));
+                        // let sFirst = await driver.findElement(By.className("p2_home")).getAttribute('innerHTML');
+                        // let sSecond = await driver.findElement(By.className("p2_away")).getAttribute('innerHTML');
+                        // firstHalfScore = sFirst.toString().trim() + '-' + sSecond.toString().trim();
+                        // console.log("First Half Score = ", firstHalfScore);
+                        //
+                        // await driver.wait(until.elementLocated(By.className("div.stage-13")));
+                        // sFirst = await driver.findElement(By.className("p2_home")).getAttribute('innerHTML');
+                        // sSecond = await driver.findElement(By.className("p2_away")).getAttribute('innerHTML');
+                        // secondHalfScore = sFirst.toString().trim() + '-' + sSecond.toString().trim();
+
+                        await driver.quit();
+                        lastId += 1;
+
+                        console.log("Category = ", sCategory, " \n sCountry = ", sCountry, "\n sLeague = ", sLeague);
+                        console.log("Match Date = ", sDate);
+                        console.log("TeamA = ", sTeamA);
+                        console.log("TeamB = ", sTeamB);
+                        console.log("FinalScore = ", finalScore);
+
+                        console.log("timeScored_firstHalf = ", timeScored_firstHalf);
+
+                        const scraping_data = new Filter({
+                            id: lastId,
+                            link: lastLink,
+                            category: sCategory,
+                            country: sCountry,
+                            league: sLeague,
+                            matchDate: sDate,
+                            teamA: sTeamA,
+                            teamB: sTeamB,
+                            finalScore: finalScore,
+                        });
+
+                        scraping_data.save();
+                        console.log(i + 1, "  -> ");
+
+                        n = sMatch.search(sId);
+                    } catch (e) {
+                        await driver.quit();
+                        n = sMatch.search(sId);
+                    }
+                }
+            } catch (e) {
+                if(error.response === undefined) {
+                    console.log("Site Error - Un-existing Url");
+                    await sleep(1000);
+                    return 0;
+                } else {
+                    console.log("Last Stage Error !!", error);
+                    await sleep(1500);
+                    await gettingCategoryLink(matchStr, bUrl);
+                }
+
+            }
+
+        } else {
+            const result = await axios.get(bUrl);
+            let $ = await cheerio.load(result.data);
+            let aStr;
+            const aTags = $(matchStr);
+
+            for (let i = 0; i < aTags.length; i++) {
+
+                aStr = aTags[i].attribs.href;
+                if (aStr !== undefined) {
+                    if((aStr.includes("2003-2004") === true) || (aStr.includes("-2004") === true)) break;
+                    mLen = goLink.length;
+                    goLink.push('https://www.diretta.it' + aStr);
+                    const scraping_Product = new ScrapingProduct({
+                        id: goLink.length,
+                        link: goLink[goLink.length - 1]
+                    });
+
+                    await scraping_Product.save();
+                    console.log(goLink.length, "  -> ", goLink[goLink.length - 1]);
+                }
+            }
+        }
     } catch (error) {
         if(error.response === undefined) {
             console.log("Site Error - Un-existing Product Url");
